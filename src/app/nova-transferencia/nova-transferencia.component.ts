3f7f5d62c8c37cb355564a8da1a0e869208efe83
Glacy <glacy.kelly3@gmail.com>
2022-07-18 23:14:23 -0300

    adicionando metodo transferir()

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  transferir() {
+    console.log('Solicitada nova transferência');
   }
-
 }
